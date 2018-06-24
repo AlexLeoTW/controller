fc8b658da91c3e7b8b4989227d6a69ffb8cf4374
AlexLeo <alex@alexleo.click>
2018-06-24 11:07:58 +0800

    start restServer

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const request = require('request');
 const environment = require('./lib/environment');
 const config      = new (require('./lib/config').Config)();
 const download    = require('./lib/download');
-const restCtrl    = require('./lib/restCtrl');
+const restServer    = new (require('./lib/restServer').RESTServer)(config);
 const DummyServer = require('./lib/dummyServer').DummyServer;
 const BungeeCtrl  = require('./lib/bungeeCtrl').BungeeCtrl;
+const linodeCtrl  = new (require('./lib/linodeCtrl').LinodeCtrl)(config.linodeToken);
 
 var dummyServer;
 var bungeeCord;
@@ -46,6 +47,7 @@ new Promise((resolve, reject) => {
   dummyServer.on('login', (client) => {
     console.log(`[${(new Date()).toLocaleTimeString()}] ${client.username} login!`);
     // trigger remote server creation
+    linodeCtrl.createGameServer(options, callbackIp);
   })
 })
 
@@ -53,57 +55,27 @@ new Promise((resolve, reject) => {
 .then(() => {
   console.log('starting bungeeCord');
   bungeeCord = new BungeeCtrl('./bungee/BungeeCord.jar', config.bungeeServer);
-  bungeeCord.start();
+  return bungeeCord.start();
 })
 
-// start restCtrl
-then(() => {
-  restCtrl.listen(config.callbackPort)
-  .on('serverReady', (ip) => {
+// start restServer
+.then(() => {
+  restServer.start();
+  restServer.on('serverReady', (ip) => {
     dummyServer.broadcast('game server is ready!!, ');
     dummyServer.broadcast('please try re-login');
 
-    var serverList = bungeeCord.getServers();
-    serverList.server = {
-      motd: 'primary server',   // TODO: make this a variable
-      address: `${ip}:25566`,
-      restricted: false
-    };
-    bungeeCord.setServers(serverList);
+    bungeeCord.stop()
+    .then(() => {
+      bungeeCord.setServer({
+        motd:       config.motd.server,
+        address:    `${ip}:${config.serverPort}`,
+        restricted: false
+      })
+    })
+    .then(() => { return bungeeCord.start() });
   })
   .on('serverStop', (ip) => {
     console.log('remote server auto-stop');
   })
 })
-
-
-function launchLinode() {
-  return new Promise((resolve, reject) => {
-    request({
-        url: 'https://api.linode.com/v4/linode/instances',
-        method: "POST",
-        headers: {
-          Authorization: 'Bearer 61246f3cc4445a8cf1089ab6818169daab8b37b3e2d02bb706b192e8c23ee3b4'    // TODO: bake this into config
-        },
-        json: {
-          image: "linode/ubuntu16.04lts",
-          stackscript_id: 316534,    // TODO: upload this yourself
-          root_pass: 'root_pass',    // TODO: bake this into config
-          stackscript_data: {
-            hostname: 'Minecraft',
-            callback: '172.105.197.147:8443'    // TODO: bake this into config
-          },
-          booted: true,
-          label: 'gameServer',
-          region: "ap-northeast",
-          type: "g6-standard-2"
-        }
-    }, (error, response, body) => {
-      if (error || response.statusCode !== 200 ) {
-        console.log(`error when creating remote server: ${error}`);
-      }
-      console.log(body);
-      resolve(body.ipv4[0]);
-    })
-  })
-}
